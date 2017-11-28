fa8e3f713e30d07dee186565b7026d4c757ec292
janvokas <vokas@vshosting.cz>
2017-11-28 12:09:21 +0100

    first

---
+++ README.md
@@ -0,0 +1,74 @@
+Python script for gathering metrics of containers on lxd host.
+
+
+Script is periodically triggered by telegraf and gathered metrics are sent to
+influxdb (and/or another metricsdb, based on telegraf configuration).
+
+
+grafana_dashboards contains example dashbords which using gathered metrics.
+
+
+This is still WORK IN PROGRESS (or proof of concept maybe).
+
+
+I'm testing this on Ubuntu LTS 16.04 with LXD version 2.20-0ubuntu4~16.04.1~ppa1, kernel 4.10.0-40-generic.
+
+
+Howto make it work (ubuntu server, for other distro use appropriate tools):
+0) install lxd server https://linuxcontainers.org/lxd/introduction/
+0.1) i have spotted that this script not working with snap version of LXD - use PPA or Backports
+1) install telegraf https://docs.influxdata.com/telegraf/v1.4/introduction/installation/
+2) install influxdb https://docs.influxdata.com/influxdb/v1.3/introduction/installation/
+3) install grafana http://docs.grafana.org/installation/debian/
+4) configure telegraf to use influxdb
+4.1) configure the running period for telegraf - default is 10s which should be too often (and may broke things)
+5) copy lxd.conf to /etc/telegraf/telegraf.d/
+6) copy sudoers telegraf to /etc/sudoers.d/
+7) copy lxd-telegraf-stats.py to /usr/local/sbin/ and chmod +x it.
+8) install additional python modules: apt-get install python-ws4py python-pylxd (maybe others? look at errors if something missing)
+9) try to run script - output should be like: 
+'''
+/usr/local/sbin/lxd-telegraf-stats.py
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=status running=1,processes=27,cpuprio=1024,hddprio=500
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=mem usage=64884736,usage_pct=3,limit=2147483648,peak=67051520
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=swap usage=0,peak=0
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=cpu usage=552144742062,limit=2,usage_percpu=276072371031
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=blkio bytes_total=31486976,iops_total=779,bytes_write=0,iops_write=0,bytes_read=31486976,iops_read=779
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=net,dev=lo pkts_out=788470,bytes_in=68317360,bytes_out=68317360,pkts_in=788470
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=net,dev=eth1 pkts_out=182,bytes_in=15541517,bytes_out=12836,pkts_in=182031
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=net,dev=vxlan pkts_out=2095146,bytes_in=40770984,bytes_out=85283645,pkts_in=575666
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=net,dev=eth0 pkts_out=2118158,bytes_in=97459691,bytes_out=220339657,pkts_in=677739
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=disk,dev=tmp usage=0,usage_pct=0,limit=536870912
+lxd,type=container,hostname=master-666,name=master,instance=666,metric=disk,dev=root usage=138584064,usage_pct=1,limit=10737418240
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=status running=1,processes=16,cpuprio=1024,hddprio=500
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=mem usage=47546368,usage_pct=2,limit=2147483648,peak=50176000
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=swap usage=0,peak=0
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=cpu usage=307662859002,limit=2,usage_percpu=153831429501
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=blkio bytes_total=29099008,iops_total=840,bytes_write=0,iops_write=0,bytes_read=29099008,iops_read=840
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=net,dev=lo pkts_out=0,bytes_in=0,bytes_out=0,pkts_in=0
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=net,dev=vxlan pkts_out=10998,bytes_in=32260729,bytes_out=308352,pkts_in=1084500
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=net,dev=eth0 pkts_out=32390,bytes_in=121446118,bytes_out=1910852,pkts_in=1184950
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=disk,dev=tmp usage=0,usage_pct=0,limit=536870912
+lxd,type=container,hostname=logger-666,name=logger,instance=666,metric=disk,dev=root usage=18042880,usage_pct=0,limit=10737418240
+lxd,type=master,metric=mem total=16812589056,given=25402523648,used=691441664,given_pct=151,used_pct=4
+lxd,type=master,metric=other live=1
+lxd,type=master,metric=hdd total=1990116046274,given=2033065719234,used=316979200,given_pct=102,used_pct=0
+lxd,type=master,metric=cpu given=24,total=16
+lxd,type=master,metric=containers running=5,total=6,stopped=1,notrunning=1
+'''
+10) restart telegraf: systemctl restart telegraf.service (you should test the gathering with telegraf --test command)
+11) login to your grafana and import attached dashboards from grafana_dashboards
+12) edit/tweak settings to make it work
+13) do not let your eyeballs pop out!
+14) maybe i'm missed something, so look at your logs if something went wrong.
+
+
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!                                   DISCLAIMER:                                   !!!
+!!! I'm not a programmer - so .py script is probably very ugly - you've been warned !!!
+!!!      No (or almost no) sanity checks, ugly written (can't to call it) code      !!!
+!!!    I'm not responsible for any eyeball explosion, use this on your own risk     !!!
+!!!                                       :wq                                       !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
---
+++ usr/local/sbin/lxd-telegraf-stats.py
@@ -0,0 +1,336 @@
+#!/usr/bin/env python
+
+import pylxd
+import os
+import sys
+import json
+import multiprocessing
+import subprocess
+import commands 
+import collections
+import re
+
+# pylxd dont have storage functions yet
+# so define storage pool name here
+lxdstorage = "zfs"
+outputtype = "influx"
+
+SYMBOLS = {
+    'customary'     : ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'),
+    'customary_ext' : ('byte', 'kilo', 'mega', 'giga', 'tera', 'peta', 'exa',
+                       'zetta', 'iotta'),
+    'iec'           : ('Bi', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'),
+    'iec_ext'       : ('byte', 'kibi', 'mebi', 'gibi', 'tebi', 'pebi', 'exbi',
+                       'zebi', 'yobi'),
+}
+
+# conversion of human readable to bytes
+def human2bytes(s):
+  init = s
+  num = ""
+  while s and s[0:1].isdigit() or s[0:1] == '.':
+    num += s[0]
+    s = s[1:]
+  num = float(num)
+  letter = s.strip()
+  for name, sset in SYMBOLS.items():
+    if letter in sset:
+      break
+  else:
+    if letter == 'k':
+      # treat 'k' as an alias for 'K' as per: http://goo.gl/kTQMs
+      sset = SYMBOLS['customary']
+      letter = letter.upper()
+    else:
+      raise ValueError("can't interpret %r" % init)
+  prefix = {sset[0]:1}
+  for i, s in enumerate(sset[1:]):
+    prefix[s] = 1 << (i+1)*10
+  return int(num * prefix[letter])
+
+# search for device name by the major:minor number
+#def ask_sysfs(majorminor):
+#  from glob import glob
+#  needle = "%s" % (majorminor)
+#  files = glob("/sys/class/block/*/dev")
+#  for f in files:
+#    if file(f).read().strip() == needle:
+#      return os.path.dirname(f).split('/')[-1]
+#  return None
+
+# init empty values
+client = pylxd.Client()
+lxdmetrics = {}
+globalmetrics = {}
+globalmetrics['cpu'] = {}
+globalmetrics['mem'] = {}
+globalmetrics['hdd'] = {}
+globalmetrics['other'] = {}
+globalmetrics['cpu']['given'] = 0
+globalmetrics['mem']['given'] = 0
+globalmetrics['hdd']['given'] = 0
+globalmetrics['mem']['used'] = 0
+globalmetrics['hdd']['used'] = 0
+globalmetrics['containers'] = {}
+globalmetrics['containers']['total'] = 0
+globalmetrics['containers']['running'] = 0
+globalmetrics['other']['live'] = 1
+
+# some global metrics for bare metal
+globalmetrics['cpu']['total'] = multiprocessing.cpu_count()
+globalmetrics['mem']['total'] = mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
+try:
+  globalmetrics['hdd']['total'] = human2bytes(commands.getoutput("zpool list " + lxdstorage + " | grep " + lxdstorage + " | awk -F ' +' '{print $2}'")+'B')
+except:
+  # storage not found, or we dont have zfs storage
+  globalmetrics['hdd']['total'] = 1
+  pass
+
+# fetch all containers
+for container in client.containers.all():
+  cno = container.state()	# cno = cno
+  
+  # counters
+  try:
+    globalmetrics['containers']['total'] += 1
+  except KeyError:
+    globalmetrics['containers']['total'] = 1
+    pass
+
+  try:
+    globalmetrics['containers'][cno.status.lower()] += 1
+  except KeyError:
+    globalmetrics['containers'][cno.status.lower()] = 1
+    pass
+
+  # list init
+  cn = container.name.lower()	# cn = containername
+  lxdmetrics[cn] = {}
+  
+  # set container status to 1 if running, otherwise 0 - so in grafana you should display this (or use it as alert)
+  if cno.status.lower() == "running":
+    lxdmetrics[cn]['running'] = 1
+  else:
+    lxdmetrics[cn]['running'] = 0
+    # if container is not running continue to next element in for loop
+    continue
+
+  # metrics
+  lxdmetrics[cn]['mem'] = {}
+  lxdmetrics[cn]['swap'] = {}
+  lxdmetrics[cn]['cpu'] = {}
+  lxdmetrics[cn]['disk'] = {}
+  lxdmetrics[cn]['blkio'] = {}
+  lxdmetrics[cn]['processes'] = cno.processes
+  lxdmetrics[cn]['mem']['usage'] = cno.memory['usage']
+  lxdmetrics[cn]['mem']['peak'] = cno.memory['usage_peak']
+  lxdmetrics[cn]['swap']['usage'] = cno.memory['swap_usage']
+  lxdmetrics[cn]['swap']['peak'] = cno.memory['swap_usage_peak']
+  lxdmetrics[cn]['cpu']['usage'] = cno.cpu['usage']
+  globalmetrics['mem']['used'] += lxdmetrics[cn]['mem']['usage']
+
+  # exec metrics (metric fetched via lxc exec direct from inside container)
+  # Not Working right now - maybe rework to try/except, just commenting it out for now
+  # /tmp size and usage
+  #ccommand = "df /tmp"
+  #ce = container.execute(ccommand.split())
+  # class values: exit_code stdout stderr
+  #if ce.exit_code == 0:
+  #  for line in ce.stdout.split('\n'):
+  #    if re.match("(.*)/tmp$", line):
+  #      lxdmetrics[cn]['disk']['tmp'] = {}
+  #      lxdmetrics[cn]['disk']['tmp']['usage'] = int(line.split()[2])*1024
+  #      lxdmetrics[cn]['disk']['tmp']['limit'] = int(line.split()[3])*1024
+  #      lxdmetrics[cn]['disk']['tmp']['usage_pct'] = int(lxdmetrics[cn]['disk']['tmp']['usage'] * 100 / lxdmetrics[cn]['disk']['tmp']['limit'])
+
+  # try/except metrics
+  try:
+    lxdmetrics[cn]['mem']['limit'] = human2bytes(container.expanded_config['limits.memory'])
+  except:
+    # if there is no limit - container should use all available resources
+    lxdmetrics[cn]['mem']['limit'] = globalmetrics['mem']['total']
+    pass
+  lxdmetrics[cn]['mem']['usage_pct'] = int(lxdmetrics[cn]['mem']['usage'] * 100 / lxdmetrics[cn]['mem']['limit'])
+  globalmetrics['mem']['given'] += lxdmetrics[cn]['mem']['limit']
+
+  # TODO - polishing for multiple storage pools
+  try:
+    for disk in cno.disk:
+      lxdmetrics[cn]['disk'][disk] = {}
+      lxdmetrics[cn]['disk'][disk]['usage'] =  cno.disk[disk]['usage']
+      try:
+        lxdmetrics[cn]['disk'][disk]['limit'] =  human2bytes(container.expanded_devices[disk]['size'])
+      except:
+        # if there is no disk limit - container shoud use all available disk space
+        lxdmetrics[cn]['disk'][disk]['limit'] = globalmetrics['hdd']['total']
+        pass
+      lxdmetrics[cn]['disk'][disk]['usage_pct'] = int(lxdmetrics[cn]['disk'][disk]['usage'] * 100 / lxdmetrics[cn]['disk'][disk]['limit'])
+      globalmetrics['hdd']['given'] += lxdmetrics[cn]['disk'][disk]['limit']
+  except:
+    pass
+
+  try:
+    globalmetrics['hdd']['used'] += lxdmetrics[cn]['disk'][disk]['usage']
+  except:
+    # container doesnt have own disk device - we cannot determine used space
+    globalmetrics['hdd']['used'] += 1
+
+  try:
+    lxdmetrics[cn]['cpu']['limit'] = int(container.expanded_config['limits.cpu'])
+  except:
+    # if there is no cpu limit - container should use all available resources
+    lxdmetrics[cn]['cpu']['limit'] = globalmetrics['cpu']['total']
+    pass
+  lxdmetrics[cn]['cpu']['usage_percpu'] = int(round(lxdmetrics[cn]['cpu']['usage'] / lxdmetrics[cn]['cpu']['limit']))
+  globalmetrics['cpu']['given'] += lxdmetrics[cn]['cpu']['limit']
+  
+  try:
+    lxdmetrics[cn]['net'] = {}
+    for interface in cno.network:
+      lxdmetrics[cn]['net'][interface] = {}
+      lxdmetrics[cn]['net'][interface]['pkts_out'] = cno.network[interface]['counters']['packets_sent']
+      lxdmetrics[cn]['net'][interface]['pkts_in'] = cno.network[interface]['counters']['packets_received']
+      lxdmetrics[cn]['net'][interface]['bytes_out'] = cno.network[interface]['counters']['bytes_sent']
+      lxdmetrics[cn]['net'][interface]['bytes_in'] = cno.network[interface]['counters']['bytes_received']
+  except:
+    pass
+  
+  # cgroup metrics - read them only when container is running
+  if lxdmetrics[cn]['running'] == 1:
+    if os.path.exists('/sys/fs/cgroup/cpu,cpuacct/lxc/%s/cpu.shares' % cn):
+      try:
+        with open('/sys/fs/cgroup/cpu,cpuacct/lxc/%s/cpu.shares' % cn, 'rt') as cgfile:
+          lxdmetrics[cn]['cpuprio'] = int(cgfile.read())
+      except:
+        pass
+  
+    if os.path.exists('/sys/fs/cgroup/blkio/lxc/%s/blkio.weight' % cn):
+      try:
+        with open('/sys/fs/cgroup/blkio/lxc/%s/blkio.weight' % cn, 'rt') as cgfile:
+          lxdmetrics[cn]['hddprio'] = int(cgfile.read())
+      except:
+        pass
+
+    if os.path.exists('/sys/fs/cgroup/blkio/lxc/%s/blkio.throttle.io_serviced' % cn):
+      try:
+        with open('/sys/fs/cgroup/blkio/lxc/%s/blkio.throttle.io_serviced' % cn, 'rt') as cgfile:
+          for line in cgfile.readlines():
+            # sum every read or write occuring
+            if "Read" in line:
+              #device = ask_sysfs(line.split()[0])
+              try: 
+                lxdmetrics[cn]['blkio']['iops_read'] += int(line.split()[2])
+              except KeyError:
+                lxdmetrics[cn]['blkio']['iops_read'] = int(line.split()[2])
+                pass
+            if "Write" in line:
+              #device = ask_sysfs(line.split()[0])
+              try: 
+                lxdmetrics[cn]['blkio']['iops_write'] += int(line.split()[2])
+              except KeyError:
+                lxdmetrics[cn]['blkio']['iops_write'] = int(line.split()[2])
+                pass
+            if "Total" in line.split()[0]:
+              #device = ask_sysfs(line.split()[0])
+              try:
+                lxdmetrics[cn]['blkio']['iops_total'] += int(line.split()[1])
+              except KeyError:
+                lxdmetrics[cn]['blkio']['iops_total'] = int(line.split()[1])
+                pass
+      except:
+        pass
+  
+    if os.path.exists('/sys/fs/cgroup/blkio/lxc/%s/blkio.throttle.io_service_bytes' % cn):
+      try:
+        with open('/sys/fs/cgroup/blkio/lxc/%s/blkio.throttle.io_service_bytes' % cn, 'rt') as cgfile:
+          for line in cgfile.readlines():
+            if "Read" in line:
+              #device = ask_sysfs(line.split()[0])
+              try:
+                lxdmetrics[cn]['blkio']['bytes_read'] += int(line.split()[2])
+              except KeyError: 
+                lxdmetrics[cn]['blkio']['bytes_read'] = int(line.split()[2])
+                pass
+            if "Write" in line:
+              #device = ask_sysfs(line.split()[0])
+              try:
+                lxdmetrics[cn]['blkio']['bytes_write'] += int(line.split()[2])
+              except KeyError:
+                lxdmetrics[cn]['blkio']['bytes_write'] = int(line.split()[2])
+                pass
+            if "Total" in line.split()[0]:
+              #device = ask_sysfs(line.split()[0])
+              try:
+                lxdmetrics[cn]['blkio']['bytes_total'] += int(line.split()[1])
+              except KeyError:
+                lxdmetrics[cn]['blkio']['bytes_total'] = int(line.split()[1])
+                pass
+      except:
+        pass
+
+  # cgroup end
+  # metrics end
+
+# some global metrics count:
+globalmetrics['mem']['used_pct'] = int(globalmetrics['mem']['used'] * 100 / globalmetrics['mem']['total'])
+globalmetrics['mem']['given_pct'] = int(globalmetrics['mem']['given'] * 100 / globalmetrics['mem']['total'])
+globalmetrics['hdd']['used_pct'] = int(globalmetrics['hdd']['used'] * 100 / globalmetrics['hdd']['total'])
+globalmetrics['hdd']['given_pct'] = int(globalmetrics['hdd']['given'] * 100 / globalmetrics['hdd']['total'])
+globalmetrics['containers']['notrunning'] = globalmetrics['containers']['total'] - globalmetrics['containers']['running']
+
+if outputtype == "json":
+  print json.dumps(lxdmetrics)
+  print json.dumps(globalmetrics)
+  sys.exit()
+
+# influx output
+output = []
+# loop for containers metrics
+for container, metric in lxdmetrics.iteritems():
+    header = "lxd,type=container,hostname=" + container
+    try:
+      # try to split hostname to name and instance by dash delimiter
+      header += ",name=" + container.split('-')[0] + ",instance=" + container.split('-')[1]
+    except:
+      #IndexError (probably)
+      # if error, keep it as is so this script shoudl work in default environment out of the zerops
+      pass
+    if metric['running'] == 1:
+      output.append(header + ",metric=status running=" + str(metric['running']) + ",processes=" + str(metric['processes']) + ",cpuprio=" + str(metric['cpuprio']) + ",hddprio=" + str(metric['hddprio']))
+      # metrics with only one level inside dictionary
+      for onelevel in ["mem", "swap", "cpu", "blkio"]:
+        metricarr = []
+        for key, value in metric[onelevel].iteritems():
+          metricarr.append(key + "=" + str(value))
+	# output definition for single metric
+        output.append(header + ",metric=" + onelevel + " " + ','.join(metricarr))
+      # metrics with two levels inside dictionary
+      for twolevel in ["net", "disk"]:
+        for key, value in metric[twolevel].iteritems():
+          metricarr = []
+          for m,v in value.iteritems():
+            metricarr.append(str(m) + "=" + str(v))
+          output.append(header + ",metric=" + twolevel + ",dev=" + key  + " " + ','.join(metricarr))
+    else:
+      # if container is not running, we will send only status metric
+      output.append(header + ",metric=status running=" + str(metric['running']))
+
+# loop for master metrics
+for metric, value in globalmetrics.iteritems():
+    header = "lxd,type=master"
+    # metrics with only one leddvel inside dictionary
+    metricarr = []
+    for key, value in value.iteritems():
+      metricarr.append(key + "=" + str(value))
+    output.append(header + ",metric=" + metric + " " + ','.join(metricarr))
+
+print '\n'.join(output)
+
+try:
+    sys.stdout.close()
+except:
+    pass
+try:
+    sys.stderr.close()
+except:
+    pass
